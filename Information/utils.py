dcf191fa17340cd49226566088235c2d0dc46ab3
jayliu <879303561@qq.com>
2023-03-14 23:07:45 +0800

    add_info

--- Information/utils.py
+++ Information/utils.py
@@ -27,3 +27,58 @@ def create_iformation_entropy(df, columns):
     df["entropy_val"] = entropy_vals
     return df
 
+def conditional_entropy(df, x_col, y_col):
+    df_xgb = df.groupby(by=x_col)[x_col].count()
+    total_x_cnt = sum(df_xgb)
+    df_xrt = pd.DataFrame()
+    df_xrt["prob_x"] = df_xgb.values / total_x_cnt
+    df_xrt[x_col] = df_xgb.index
+
+    x_unival = df[x_col].unique()
+    entropy_vals = []
+    for x_c in x_unival:
+        df_y = df[df[x_col] == x_c][[y_col]]
+        entropy_y_val = Information_entropy(infomation_val(df_y[[y_col]], y_col))
+        entropy_vals.append(entropy_y_val)
+    df_entrpy_y = pd.DataFrame()
+    df_entrpy_y[x_col] = x_unival
+    df_entrpy_y["condtion_entrpy"] = entropy_vals
+    df_rslt = pd.merge(df_entrpy_y, df_xrt, how="left", right_on=x_col, left_on=x_col)
+    df_rslt["val"] = df_rslt["condtion_entrpy"] * df_rslt["prob_x"]
+    return round(df_rslt["val"].sum(), 4)
+
+def union_entrpy(df, x_col, y_col):
+    entropy_x_val = Information_entropy(infomation_val(df[[x_col]], x_col))
+    conditi_entropy = conditional_entropy(df, x_col, y_col)
+    return round(entropy_x_val + conditi_entropy, 5)
+
+def mutual_information(df, x_col, y_col):
+    entropy_y = Information_entropy(infomation_val(df[[y_col]], y_col))
+    condi_val = conditional_entropy(df, x_col=y_col, y_col=x_col)
+    return round(entropy_y - condi_val, 5)
+
+
+def relative_entropy(df, x_col, y_col):
+    df_gb_y = df.groupby(by=y_col)[y_col].count()
+    total_cnt = sum(df_gb_y)
+    df_rt_y = round(df_gb_y / total_cnt, 5)
+    df_rslt_Y = pd.DataFrame()
+    df_rslt_Y["info_val"] = df_rt_y.map(lambda x: - math.log2(x))
+    df_rslt_Y["prob"] = df_rt_y
+
+    df_gb_x = df.groupby(by=x_col)[x_col].count()
+    total_cnt = sum(df_gb_x)
+    df_rt_x = round(df_gb_x / total_cnt, 5)
+
+    df_rslt_Y["p"] = df_rt_x.values
+    df_rslt_Y["rslt"] = df_rslt_Y["p"] * df_rslt_Y["info_val"]
+    return round(df_rslt_Y["rslt"].sum(), 5)
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pandas as pd
-from Information.utils import Information_entropy, infomation_val, create_iformation_entropy
+from Information.utils import Information_entropy, infomation_val, create_iformation_entropy, conditional_entropy, union_entrpy, mutual_information, relative_entropy
 from jay_plot.utils import plot_two
 
 
 df = pd.read_csv("data/train.csv")
+
 # df_rslt = infomation_val(df, "area_id")
 # plot_two(df_rslt, "area_id", "info_val", "prob")
-df_entrpy = create_iformation_entropy(df, df.columns)
-print(df_entrpy.sort_values(by="entropy_val"))
+# df_entrpy = create_iformation_entropy(df, df.columns)
+# print(df_entrpy.sort_values(by="entropy_val"))
+
+
+for col in ["main_account_active_loan_no", "passport_flag", "Driving_flag"]:
+    val = conditional_entropy(df, x_col=col,  y_col="loan_default")
+    union_val = union_entrpy(df, x_col=col,  y_col="loan_default")
+    mutual_val = mutual_information(df, x_col="loan_default",  y_col=col)
+    KL = relative_entropy(df, x_col=col,  y_col="loan_default")
+    print(KL, col)
+
 
